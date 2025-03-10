7f8742d9b7e9055ad17b7836c73f264de012bcac
Kacper <kacper.pelka03@gmail.com>
2025-03-10 12:38:59 +0100

    Add horizontal divider above navigation bar

--- mobile/app/src/main/java/com/example/socialmeetingapp/presentation/navigation/NavigationBar.kt
+++ mobile/app/src/main/java/com/example/socialmeetingapp/presentation/navigation/NavigationBar.kt
@@ -1,21 +1,19 @@
 package com.example.socialmeetingapp.presentation.navigation
 
 import android.net.Uri
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Notifications
-import androidx.compose.material.icons.outlined.Add
 import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Notifications
 import androidx.compose.material3.Badge
 import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBarItem
@@ -30,73 +28,77 @@ import com.example.socialmeetingapp.presentation.common.Routes
 
 @Composable
 fun NavigationBar(currentRoute: Routes, onItemClicked: (Routes) -> Unit, profileID: String, profileImageUrl: Uri, notReadNotifications: Int = 0) {
-    androidx.compose.material3.NavigationBar(
-        containerColor = MaterialTheme.colorScheme.background,
-        tonalElevation = 0.dp
-    ) {
-        NavigationBarItem(
-            icon = {
-                Icon(
-                    imageVector = if (currentRoute is Routes.Map) Icons.Default.Home else Icons.Outlined.Home,
-                    contentDescription = "Search"
-                )
-            },
-            selected = currentRoute is Routes.Map,
-            onClick = {
-                onItemClicked(Routes.Map())
-            }
-        )
-        NavigationBarItem(
-            icon = {
-                Icon(
-                    imageVector = if (currentRoute == Routes.Activities) Icons.Filled.Favorite else Icons.Outlined.FavoriteBorder,
-                    contentDescription = "Features"
-                )
-            },
-            selected = currentRoute == Routes.Activities,
-            onClick = {
-                onItemClicked(Routes.Activities)
-            }
-        )
-
-        NavigationBarItem(
-            icon = {
-                BadgedBox(badge = {
-                    if (notReadNotifications > 0) {
-                        Badge {
-                            Text(text = notReadNotifications.toString())
-                        }
-                    }
-                }) {
+    Column {
+        HorizontalDivider()
+        androidx.compose.material3.NavigationBar(
+            containerColor = MaterialTheme.colorScheme.background,
+            tonalElevation = 0.dp
+        ) {
+            NavigationBarItem(
+                icon = {
                     Icon(
-                        imageVector = if (currentRoute == Routes.Notifications) Icons.Filled.Notifications else Icons.Outlined.Notifications,
-                        contentDescription = "Settings"
+                        imageVector = if (currentRoute is Routes.Map) Icons.Default.Home else Icons.Outlined.Home,
+                        contentDescription = "Search"
                     )
+                },
+                selected = currentRoute is Routes.Map,
+                onClick = {
+                    onItemClicked(Routes.Map())
                 }
-            },
-            selected = currentRoute == Routes.Notifications,
-            onClick = {
-                onItemClicked(Routes.Notifications)
-            }
-        )
+            )
+            NavigationBarItem(
+                icon = {
+                    Icon(
+                        imageVector = if (currentRoute == Routes.Activities) Icons.Filled.Favorite else Icons.Outlined.FavoriteBorder,
+                        contentDescription = "Features"
+                    )
+                },
+                selected = currentRoute == Routes.Activities,
+                onClick = {
+                    onItemClicked(Routes.Activities)
+                }
+            )
+
+            NavigationBarItem(
+                icon = {
+                    BadgedBox(badge = {
+                        if (notReadNotifications > 0) {
+                            Badge {
+                                Text(text = notReadNotifications.toString())
+                            }
+                        }
+                    }) {
+                        Icon(
+                            imageVector = if (currentRoute == Routes.Notifications) Icons.Filled.Notifications else Icons.Outlined.Notifications,
+                            contentDescription = "Settings"
+                        )
+                    }
+                },
+                selected = currentRoute == Routes.Notifications,
+                onClick = {
+                    onItemClicked(Routes.Notifications)
+                }
+            )
 
 
-        NavigationBarItem(
-            icon = {
-                AsyncImage(
-                    model = profileImageUrl,
-                    contentDescription = "Profile Picture",
-                    modifier = Modifier
-                        .size(24.dp).clip(RoundedCornerShape(12.dp)),
-                    contentScale = ContentScale.Crop,
+            NavigationBarItem(
+                icon = {
+                    AsyncImage(
+                        model = profileImageUrl,
+                        contentDescription = "Profile Picture",
+                        modifier = Modifier
+                            .size(24.dp)
+                            .clip(RoundedCornerShape(12.dp)),
+                        contentScale = ContentScale.Crop,
 
-                )
-            },
-            selected = currentRoute == Routes.Profile(profileID),
-            onClick = {
-                onItemClicked(Routes.Profile(profileID))
-            }
-        )
+                        )
+                },
+                selected = currentRoute == Routes.Profile(profileID),
+                onClick = {
+                    onItemClicked(Routes.Profile(profileID))
+                }
+            )
+        }
     }
 }
 
